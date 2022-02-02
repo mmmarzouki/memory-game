b7f0742b728f74d474405151de6ef5812b24d5f6
mmmarzouki <mmmarzouki@gmailcom>
2022-02-02 18:02:10 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Game } from './components/Game/Game';
+
+function App() {
+  return (
+    <div className="App">
+      <Game></Game>
+    </div>
+  );
+}
+
+export default App;
